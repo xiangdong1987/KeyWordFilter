f15e6c5d5b865e04c08369e889aaed2a864dc460
xiangdongdong <xiangdongdong@chelun.com>
2019-11-08 16:45:13 +0800

    1.首次提交

---
+++ src/KeyWordFilter.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xiangdongdong
+ * Date: 2019/11/8
+ * Time: 14:00
+ */
+
+namespace Hyperf\KeyWordFilter;
+
+class KeyWordFilter
+{
+    /**
+     * @var TrieTree
+     */
+    public $trieTreeList;
+    public $config;
+
+    function __construct($config)
+    {
+        $this->config = $config;
+        //初始化词库
+        $dictionaryPath = $this->config['dictionaryPath'];
+        if ($dictionaryPath) {
+            foreach ($dictionaryPath as $name => $path) {
+                $this->trieTreeList[$name] = new TrieTree($path);
+            }
+        } else {
+            $message = "dictionary path didn't set !";
+            $code = '404';
+            throw new \Exception($message, $code);
+        }
+    }
+
+    public function search($dic, $str)
+    {
+        if (isset($this->trieTreeList[$dic])) {
+            $tree = $this->trieTreeList[$dic];
+            return $tree->search($str);
+        } else {
+            $message = "dictionary is not exist!";
+            $code = '404';
+            throw new \Exception($message, $code);
+        }
+    }
+}
---
+++ src/KeyWordFilterFactory.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xiangdongdong
+ * Date: 2019/11/8
+ * Time: 14:00
+ */
+
+namespace Hyperf\KeyWordFilter;
+
+use Psr\Container\ContainerInterface;
+use Hyperf\Contract\ConfigInterface;
+
+class KeyWordFilterFactory
+{
+    public function __invoke(ContainerInterface $container)
+    {
+        $config = $container->get(ConfigInterface::class);
+        $config = $config->get('server.keywordFilter');
+        if (!$config) {
+            $config = [
+                'dictionaryPath' => [
+                    'default' => BASE_PATH . "/vendor/hyperf/keyWordFilter/src/default.txt"
+                ]
+            ];
+        }
+        return new KeyWordFilter($config);
+    }
+}
